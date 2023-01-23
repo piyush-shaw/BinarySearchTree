65f5cb5184f9b5b5aa7f15a47023b64b9dc1658d
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-24 02:38:49 +0530

    [Piyush] added to search 63 in the binary tree by recursively searching left or right nodes

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         Console.WriteLine("Welcome to Binary Search Tree Problem !!! ");
         BinarySearchTree<int> binaryTree = new BinarySearchTree<int>(56);
-        Console.WriteLine("\n1.Create Binary Search Tree \n2.Size of Binary Tree");
+        Console.WriteLine("\n1.Create Binary Search Tree \n2.Size of Binary Tree \n3.Search Binary Tree");
         Console.WriteLine("Select the option from above");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -33,6 +33,22 @@ internal class Program
                 binaryTree.Insert(67);
                 binaryTree.Display();
                 binaryTree.GetSize();
+                break;
+            case 3:
+                binaryTree.Insert(30);
+                binaryTree.Insert(70);
+                binaryTree.Insert(22);
+                binaryTree.Insert(44);
+                binaryTree.Insert(60);
+                binaryTree.Insert(95);
+                binaryTree.Insert(11);
+                binaryTree.Insert(65);
+                binaryTree.Insert(3);
+                binaryTree.Insert(16);
+                binaryTree.Insert(63);
+                binaryTree.Insert(67);
+                Console.WriteLine("Searching node is present or not in binary tree");
+                bool result = binaryTree.IfExists(63, binaryTree);
                 break;
             default:
                 Console.WriteLine("Please enter the correct option");
