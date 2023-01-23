b86a2ecf2c95ea6731471894a63c08c1a668fa5b
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-24 02:15:08 +0530

    [Piyush] added to create binary tree and check if all are added using size method

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -4,6 +4,8 @@ namespace BinarySearchTree
     //Creating BST using generics and IComparable to use CompareTo method in program
     class BinarySearchTree<T> where T : IComparable<T>
     {
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
         public T NodeData { get; set; }
         public BinarySearchTree<T> leftTree { get; set; }
         public BinarySearchTree<T> rightTree { get; set; }
@@ -16,9 +18,6 @@ namespace BinarySearchTree
 
         }
 
-        int leftCount = 0, rightCount = 0;
-        bool result = false;
-
         //Method to check node and add node value to either left or right by comparing
         public void Insert(T item)
         {
@@ -26,16 +25,29 @@ namespace BinarySearchTree
             if ((currentNodeValue.CompareTo(item)) > 0)
             {
                 if (this.leftTree == null)
+                {
+                    this.leftCount++;
                     this.leftTree = new BinarySearchTree<T>(item);
+                }
                 else
+                {
+
+                    this.leftCount++;
                     this.leftTree.Insert(item);
+                }
             }
             else
             {
                 if (this.rightTree == null)
+                {
+                    this.rightCount++;
                     this.rightTree = new BinarySearchTree<T>(item);
+                }
                 else
+                {
+                    this.rightCount++;
                     this.rightTree.Insert(item);
+                }
             }
         }
 
@@ -43,18 +55,20 @@ namespace BinarySearchTree
         public void Display()
         {
             if (this.leftTree != null)
-            {
-                this.leftCount++;
+            {  
                 this.leftTree.Display();
-
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.rightTree != null)
             {
-                this.rightCount++;
                 this.rightTree.Display();
             }
         }
 
+        //Method to Get Size of the binary tree
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
     }
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         Console.WriteLine("Welcome to Binary Search Tree Problem !!! ");
         BinarySearchTree<int> binaryTree = new BinarySearchTree<int>(56);
-        Console.WriteLine("\n1.Create Binary Search Tree");
+        Console.WriteLine("\n1.Create Binary Search Tree \n2.Size of Binary Tree");
         Console.WriteLine("Select the option from above");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
@@ -16,6 +16,23 @@ internal class Program
                 binaryTree.Insert(30);
                 binaryTree.Insert(70);
                 binaryTree.Display();
+                break;
+            case 2:
+                Console.WriteLine("Inserting more node and get size of binary tree ");
+                binaryTree.Insert(30);
+                binaryTree.Insert(70);
+                binaryTree.Insert(22);
+                binaryTree.Insert(44);
+                binaryTree.Insert(60);
+                binaryTree.Insert(95);
+                binaryTree.Insert(11);
+                binaryTree.Insert(65);
+                binaryTree.Insert(3);
+                binaryTree.Insert(16);
+                binaryTree.Insert(63);
+                binaryTree.Insert(67);
+                binaryTree.Display();
+                binaryTree.GetSize();
                 break;
             default:
                 Console.WriteLine("Please enter the correct option");
